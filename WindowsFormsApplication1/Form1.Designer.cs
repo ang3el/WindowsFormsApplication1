fb00a17ec24431961c1dbac50a7d3051129b6963
ang3el <angelito7246@gmail.com>
2016-03-29 00:14:28 +0200

    sve i svasta
    neam opsja

---
+++ WindowsFormsApplication1/Form1.Designer.cs
@@ -0,0 +1,140 @@
+﻿namespace WindowsFormsApplication1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txtIme = new System.Windows.Forms.TextBox();
+            this.TxtPrezime = new System.Windows.Forms.TextBox();
+            this.txtOIB = new System.Windows.Forms.TextBox();
+            this.btnIspis = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(77, 49);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(27, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Ime:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(77, 82);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(47, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Prezime:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(77, 117);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(28, 13);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "OIB:";
+            // 
+            // txtIme
+            // 
+            this.txtIme.Location = new System.Drawing.Point(145, 49);
+            this.txtIme.Name = "txtIme";
+            this.txtIme.Size = new System.Drawing.Size(100, 20);
+            this.txtIme.TabIndex = 3;
+            // 
+            // TxtPrezime
+            // 
+            this.TxtPrezime.Location = new System.Drawing.Point(145, 82);
+            this.TxtPrezime.Name = "TxtPrezime";
+            this.TxtPrezime.Size = new System.Drawing.Size(100, 20);
+            this.TxtPrezime.TabIndex = 4;
+            // 
+            // txtOIB
+            // 
+            this.txtOIB.Location = new System.Drawing.Point(145, 117);
+            this.txtOIB.Name = "txtOIB";
+            this.txtOIB.Size = new System.Drawing.Size(100, 20);
+            this.txtOIB.TabIndex = 5;
+            // 
+            // btnIspis
+            // 
+            this.btnIspis.Location = new System.Drawing.Point(80, 189);
+            this.btnIspis.Name = "btnIspis";
+            this.btnIspis.Size = new System.Drawing.Size(98, 28);
+            this.btnIspis.TabIndex = 6;
+            this.btnIspis.Text = "Ispisi";
+            this.btnIspis.UseVisualStyleBackColor = true;
+            this.btnIspis.Click += new System.EventHandler(this.btnIspis_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(349, 137);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(200, 151);
+            this.textBox1.TabIndex = 7;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(573, 318);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.btnIspis);
+            this.Controls.Add(this.txtOIB);
+            this.Controls.Add(this.TxtPrezime);
+            this.Controls.Add(this.txtIme);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txtIme;
+        private System.Windows.Forms.TextBox TxtPrezime;
+        private System.Windows.Forms.TextBox txtOIB;
+        private System.Windows.Forms.Button btnIspis;
+        private System.Windows.Forms.TextBox textBox1;
+    }
+}
+
---
+++ WindowsFormsApplication1/Lista.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WindowsFormsApplication1
+{
+    class Lista
+    {
+        public string ime  { get; set; }
+        public string prezime { get; set; }
+        public string oib { get; set; }
+        public Lista()
+        {
+            
+        }
+        
+        public void unos(string i, string p, string o)
+        {
+            ime = i;
+            prezime = p;
+            oib = o;
+        }
+        public string ispis()
+        {
+            string osobe ="";
+            osobe = ime + " " + prezime + " "+ oib + "\r\n";
+            return osobe;
+            
+        }
+    }
+}
